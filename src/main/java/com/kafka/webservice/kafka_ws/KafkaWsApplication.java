a60fb1f8cb3420d90a8de9a2895f48652439ee06
djarallah <djarallah.brahim@gmail.com>
2024-10-05 08:59:59 +0200

    init commit

---
+++ src/main/java/com/kafka/webservice/kafka_ws/KafkaWsApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.webservice.kafka_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/webservice/kafka_ws/KafkaWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.webservice.kafka_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
