3965e039dff0501b8032fa9418400cee5d20136c
djarallah <djarallah.brahim@gmail.com>
2024-11-02 19:50:05 +0100

    refactor project packages

--- src/main/java/com/kafka/webservice/producer/KafkaWsApplication.java
+++ src/main/java/com/kafka/webservice/producer/KafkaWsApplication.java
@@ -1,4 +1,4 @@
-package com.kafka.webservice.kafka_ws;
+package com.kafka.webservice.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
