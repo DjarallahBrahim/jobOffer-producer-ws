6924c53d181e20fa4784fa81bda28c9c5fc333f2
djarallah <djarallah.brahim@gmail.com>
2024-11-04 14:26:59 +0100

    delete post-install-script

--- docker/pre-install-scripts/prepare-catalina-base.sh
+++ docker/pre-install-scripts/prepare-catalina-base.sh
@@ -2,17 +2,18 @@
 set -e
 
 mkdir -p ${CATALINA_BASE}/{bin,conf,logs,temp,webapps,work}
+mkdir -p ${CATALINA_BASE}/logs/archives
 
 cp $CATALINA_HOME/conf/server.xml ${CATALINA_BASE}/conf
 
 cp $CATALINA_HOME/conf/web.xml ${CATALINA_BASE}/conf
 
-cp /install/config-files/tomcat/server.xml $CATALINA_BASE/conf/
+#cp /install/config-files/tomcat/server.xml $CATALINA_BASE/conf/
 
 cp $CATALINA_HOME/conf/tomcat-users.xml ${CATALINA_BASE}/conf
 
 cp $CATALINA_HOME/conf/catalina.properties ${CATALINA_BASE}/conf
 
-sed -i '/^common.loader/s=.*=&,/usr/-tomcat-components2/*.jar=' ${CATALINA_BASE}/conf/catalina.properties
+#sed -i '/^common.loader/s=.*=&,/usr/-tomcat-components2/*.jar=' ${CATALINA_BASE}/conf/catalina.properties
 
-mkdir -p ${CATALINA_BASE}/webapps/bgl-gateway-api
+mkdir -p ${CATALINA_BASE}/webapps/job-offer-producer
