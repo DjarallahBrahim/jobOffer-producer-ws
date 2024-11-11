0b94c1ce8aa22abff4c9e7df3c63980dd965317c
djarallah <djarallah.brahim@gmail.com>
2024-11-11 22:29:28 +0100

    update kafka host Config

--- src/main/java/com/kafka/webservice/producer/producer/Config.java
+++ src/main/java/com/kafka/webservice/producer/producer/Config.java
@@ -20,47 +20,58 @@ import java.util.Map;
 @Configuration
 public class Config {
 
-    @Value("${topic.kafka.name}")
-    private String kafkaTopicName;
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
 
-    @Autowired
-    Environment environment;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
 
-    @Bean
-    public Map<String, Object> producerConfigs() {
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeout;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String linger;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeout;
+
+    Map<String, Object> producerConfigs() {
         Map<String, Object> config = new HashMap<>();
 
-        // Using environment.getProperty for all properties
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.producer.bootstrap-servers"));
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, environment.getProperty("spring.kafka.producer.key-serializer"));
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, environment.getProperty("spring.kafka.producer.value-serializer"));
-        config.put(ProducerConfig.ACKS_CONFIG, environment.getProperty("spring.kafka.producer.acks"));
-        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, environment.getProperty("spring.kafka.producer.properties.delivery.timeout.ms"));
-        config.put(ProducerConfig.LINGER_MS_CONFIG, environment.getProperty("spring.kafka.producer.properties.linger.ms"));
-        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, environment.getProperty("spring.kafka.producer.properties.request.timeout.ms"));
-        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_DOC, environment.getProperty("spring.kafka.producer.enable.idempotence"));
-        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, environment.getProperty("spring.kafka.producer.max.in.flight.requests.per.connection"));
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
 
-        // Return the final configuration map
         return config;
     }
 
     @Bean
-    public ProducerFactory<String, JobOfferCreatedEvent> producerFactory() {
+    ProducerFactory<String, JobOfferCreatedEvent> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 
     @Bean
-    public KafkaTemplate<String, JobOfferCreatedEvent> kafkaTemplate(ProducerFactory<String, JobOfferCreatedEvent> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+    KafkaTemplate<String, JobOfferCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<String, JobOfferCreatedEvent>(producerFactory());
     }
 
     @Bean
-    public NewTopic createTopic() {
-        return TopicBuilder.name(this.kafkaTopicName)
+    NewTopic createTopic() {
+        return TopicBuilder.name("job-offer-events-topic")
                 .partitions(3)
                 .replicas(2)
-                .config("min.insync.replicas", "2") // Ensure min.insync.replicas aligns with replication factor and broker config
+                .configs(Map.of("min.insync.replicas","2"))
                 .build();
     }
+
 }
